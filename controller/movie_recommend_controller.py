341205d69d7639e7e1e065436bc4a01320c2c594
kaibad <badukailash001@gmail.com>
2025-07-16 19:40:23 +0545

    optimize movie recommendation API

--- controller/movie_recommend_controller.py
+++ controller/movie_recommend_controller.py
@@ -2,16 +2,17 @@ from app import app
 from flask import request, jsonify
 from model.movie_recommend_model import MovieRecommendModel
 
+recommender = MovieRecommendModel()
 
 @app.route("/recommend/movie", methods=["GET"])
 def movie_recommend_controller():
     raw_title = request.args.get("title")
-    recommender = MovieRecommendModel()
+    
     if not raw_title:
-        all_movies = recommender.get_all_movie_titles()
-        return jsonify({"all_movies": all_movies})
+        all_movies = recommender.get_popular_movies()
+        return all_movies
     
     title = raw_title.strip().strip('"').strip("'")
     result = recommender.movie_recommend_model(title)
 
-    return jsonify(result)
+    return result
