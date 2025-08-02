9cc7e5f2b2e13e43622511b152210ab4f50b9a25
Kailash Badu <106222832+kaibad@users.noreply.github.com>
2025-08-02 17:29:59 +0545

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 dbconfig = {
     "host": os.getenv("DB_HOST", "localhost"),
     "port": int(os.getenv("DB_PORT", 3306)),
-    "user": os.getenv("DB_USER", "root"),
+    "username": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", ""),
     "database": os.getenv("DB_NAME", "db_sujhavmitra")
 }
